7dab9b71e2c5b12f9e0730c91ffb4e921dd90f6d
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-28 02:38:42 +0100

    added the needed modules

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AppointmentModule } from './appointment/appointment.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, AppointmentModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
