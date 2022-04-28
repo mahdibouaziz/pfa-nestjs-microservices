4ba00e3c87772c41d0dc353a27b38403fcaf3585
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-28 03:00:26 +0100

    added the appointment BD and the auth BD

--- appointment/src/app.module.ts
+++ appointment/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_LINK}/${process.env.DATABASE_NAME}?authSource=admin`,
+    ),
     ClientsModule.register([
       {
         name: 'AUTH_SERVICE',
