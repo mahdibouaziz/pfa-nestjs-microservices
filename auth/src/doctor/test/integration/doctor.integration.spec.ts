3298dd5948b5e13c353ba2a7ebc42ec48aa4d43a
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-29 18:30:18 +0100

    finished the test integration for the doctor

--- auth/src/doctor/test/integration/doctor.integration.spec.ts
+++ auth/src/doctor/test/integration/doctor.integration.spec.ts
@@ -39,7 +39,7 @@ describe('Doctor Controller', () => {
     await app.close();
   });
 
-  describe('getAllDoctors', () => {
+  describe('Login and Register Doctors', () => {
     it('login an existing doctor to the DB with admin privileges', async () => {
       // insert doctor in the DB with the right credentials
       const salt = await bcrypt.genSalt();
@@ -102,7 +102,6 @@ describe('Doctor Controller', () => {
     });
 
     it('login the doctor with valid credentials', async () => {
-      // get the admin doctor and attach to it a token
       const loginResponse = await request(httpServer)
         .post('/doctor/login')
         .send({
@@ -115,7 +114,6 @@ describe('Doctor Controller', () => {
     });
 
     it('login the doctor with wrong credentials', async () => {
-      // get the admin doctor and attach to it a token
       const loginResponse = await request(httpServer)
         .post('/doctor/login')
         .send({
@@ -128,7 +126,55 @@ describe('Doctor Controller', () => {
       expect(loginResponse.status).toBe(401);
       expect(loginResponse.body.error).toEqual('Unauthorized');
     });
+  });
 
-    // more tests
+  describe('Get all doctors', () => {
+    it('fetch the doctors - with an admin account', async () => {
+      // get the admin doctor and attach to it a token
+      const loginResponse = await request(httpServer)
+        .post('/doctor/login')
+        .send({
+          email: doctorAdminStub().email,
+          password: doctorAdminStub().password,
+        });
+
+      // get all the doctors
+      const fetchResponse = await request(httpServer)
+        .get('/doctor/all')
+        .set('Authorization', `Bearer ${loginResponse.body.accessToken}`);
+
+      expect(fetchResponse.status).toBe(200);
+      expect(fetchResponse.body.data).toBeTruthy();
+      expect(fetchResponse.body.totalItems).toBeTruthy();
+      expect(fetchResponse.body.totalPages).toBeTruthy();
+    });
+
+    it('fetch the doctors - with a doctor account', async () => {
+      // get the doctor and attach to it a token
+      const loginResponse = await request(httpServer)
+        .post('/doctor/login')
+        .send({
+          email: doctorStub().email,
+          password: doctorStub().password,
+        });
+
+      // get all the doctors
+      const fetchResponse = await request(httpServer)
+        .get('/doctor/all')
+        .set('Authorization', `Bearer ${loginResponse.body.accessToken}`);
+
+      expect(fetchResponse.status).toBe(200);
+      expect(fetchResponse.body.data).toBeTruthy();
+      expect(fetchResponse.body.totalItems).toBeTruthy();
+      expect(fetchResponse.body.totalPages).toBeTruthy();
+    });
+
+    it('fetch the doctors - without an account (must fails)', async () => {
+      // get all the doctors
+      const fetchResponse = await request(httpServer).get('/doctor/all');
+
+      expect(fetchResponse.status).toBe(401);
+      expect(fetchResponse.body.message).toEqual('Unauthorized');
+    });
   });
 });
