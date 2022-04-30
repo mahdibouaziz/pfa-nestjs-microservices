ffe4464a087a155c84530b730896acb414e34ee3
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-30 11:00:36 +0100

    fixed the register doctor

--- api-gateway/src/auth/dto/register-doctor.dto.ts
+++ api-gateway/src/auth/dto/register-doctor.dto.ts
@@ -1,10 +1,21 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class RegisterDoctorDto {
+  @IsNotEmpty()
   firstname: string;
+  @IsNotEmpty()
   lastname: string;
-  password: string;
+  @IsNotEmpty()
   phone: string;
+  @IsNotEmpty()
   birthday: Date;
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+  @IsNotEmpty()
+  password: string;
+  @IsNotEmpty()
   type: string;
+
   isAdmin: boolean;
 }
