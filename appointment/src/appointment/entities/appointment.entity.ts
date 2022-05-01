b18022748db5dd6ebd8d0fef7d6e80e93f094635
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-05-01 16:51:14 +0100

    added the appointment entity

--- appointment/src/appointment/entities/appointment.entity.ts
+++ appointment/src/appointment/entities/appointment.entity.ts
@@ -1,1 +1,47 @@
-export class Appointment {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+import * as mongoose from 'mongoose';
+
+export type AppointmentDocument = Appointment & Document;
+
+@Schema()
+export class Appointment {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+  })
+  patientId: string;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+  })
+  doctorId: string;
+
+  @Prop()
+  date: string;
+
+  @Prop()
+  time: string;
+
+  @Prop({
+    enum: ['consultation', 'hopital du jours'],
+  })
+  type: string;
+
+  @Prop({
+    enum: ['pending', 'canceled', 'done'],
+  })
+  status: string;
+
+  // replicated data
+  @Prop()
+  doctorLastname: string;
+  @Prop()
+  patientCIN: string;
+  @Prop()
+  patientBirthday: string;
+  @Prop()
+  patientName: string;
+}
+
+export const AppointmentSchema = SchemaFactory.createForClass(Appointment);
