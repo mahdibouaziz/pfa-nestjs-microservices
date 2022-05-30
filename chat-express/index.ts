399d3cfef7f3329b0dcf49294089c7237e842e58
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-05-30 10:39:54 +0100

    added the socket part

--- chat-express/index.ts
+++ chat-express/index.ts
@@ -1,15 +1,33 @@
 import express, { Express, Request, Response } from "express";
+import http from "http";
+import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config();
-
-const app: Express = express();
+const app = express();
 const port = process.env.PORT || 5000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+// middlewares
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+// socket io part
+io.on("connection", (socket: any) => {
+  console.log("connected");
+  console.log(socket.id, "Has joined");
+  socket.on("signin", (id: any) => {
+    console.log("id: ", id);
+    //
+  });
+});
+
+// http part
+server.listen(port, () => {
+  console.log("listening on *:5000");
 });
